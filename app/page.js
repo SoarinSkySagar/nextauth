c0f43d139214dca8e445341c5bb6fe1d0adb83ac
SoarinSkySagar <sagarrana.pvt@gmail.com>
2024-04-11 18:58:42 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
 import Image from "next/image";
+import { authOptions } from "./lib/nextAuth";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(authOptions)
+
+  console.log({homepage: session})
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
